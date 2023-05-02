c540f5c134700fb82732b5c60a14b78cf8e0db80
nagadurgaprasadkotthuru <125784996+nagadurgaprasadkotthuru@users.noreply.github.com>
2023-05-02 22:21:14 +0530

    third_commit

--- src/components/VaccinationCoverage/index.js
+++ src/components/VaccinationCoverage/index.js
@@ -14,39 +14,42 @@ const VaccinationCoverage = props => {
   const {CoverageList} = props
 
   return (
-    <ResponsiveContainer
-      width={1000}
-      height={300}
-      className="responsive-container"
-    >
-      <BarChart
-        data={CoverageList}
-        margin={{
-          top: 5,
-        }}
+    <>
+      <h1 className="heading">Vaccination Coverage</h1>
+      <ResponsiveContainer
+        width={1000}
+        height={300}
+        className="responsive-container"
       >
-        <XAxis
-          dataKey="vaccineDate"
-          tick={{
-            stroke: 'gray',
-            strokeWidth: 1,
+        <BarChart
+          data={CoverageList}
+          margin={{
+            top: 5,
           }}
-        />
-        <YAxis
-          tick={{
-            stroke: 'gray',
-            strokeWidth: 0,
-          }}
-        />
-        <Legend
-          wrapperStyle={{
-            padding: 30,
-          }}
-        />
-        <Bar dataKey="dose1" name="Dose 1" fill="#2d87bb" barSize="5%" />
-        <Bar dataKey="dose2" name="Dose 2" fill="#f54394" barSize="5%" />
-      </BarChart>
-    </ResponsiveContainer>
+        >
+          <XAxis
+            dataKey="vaccineDate"
+            tick={{
+              stroke: 'gray',
+              strokeWidth: 1,
+            }}
+          />
+          <YAxis
+            tick={{
+              stroke: 'gray',
+              strokeWidth: 0,
+            }}
+          />
+          <Legend
+            wrapperStyle={{
+              padding: 30,
+            }}
+          />
+          <Bar dataKey="dose1" name="Dose 1" fill="#2d87bb" barSize="5%" />
+          <Bar dataKey="dose2" name="Dose 2" fill="#f54394" barSize="5%" />
+        </BarChart>
+      </ResponsiveContainer>
+    </>
   )
 }
 
