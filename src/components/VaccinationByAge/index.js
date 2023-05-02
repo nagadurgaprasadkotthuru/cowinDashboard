2835feb331ec7e67e55029f3a9e0ce2718db0ac7
nagadurgaprasadkotthuru <125784996+nagadurgaprasadkotthuru@users.noreply.github.com>
2023-05-02 22:22:40 +0530

    third_commit

--- src/components/VaccinationByAge/index.js
+++ src/components/VaccinationByAge/index.js
@@ -2,33 +2,46 @@ import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
 
 import './index.css'
 
+const colorsList = ['#2d87bb', '#a3df9f', '#64c2a6']
+
 const VaccinationByAge = props => {
   const {byAgeList} = props
   return (
-    <ResponsiveContainer width="100%" height={300}>
-      <PieChart>
-        <Pie
-          cx="100%"
-          cy="0%"
-          data={byAgeList}
-          startAngle={0}
-          endAngle={360}
-          innerRadius="0%"
-          outerRadius="100%"
-          dataKey="count"
-        >
-          {byAgeList.map(eachItem => (
-            <Cell name={eachItem.age} fill="#fecba6" />
-          ))}
-        </Pie>
-        <Legend
-          iconType="circle"
-          layout="vertical"
-          verticalAlign="middle"
-          align="right"
-        />
-      </PieChart>
-    </ResponsiveContainer>
+    <div className="vaccination-by-age-container">
+      <h1 className="heading">Vaccination by Age</h1>
+      <ResponsiveContainer
+        width="100%"
+        height={200}
+        className="vaccination-by-age-container1"
+      >
+        <PieChart className="pie-char-container">
+          <Pie
+            cx="70%"
+            cy="40%"
+            data={byAgeList}
+            startAngle={0}
+            endAngle={360}
+            innerRadius="0%"
+            outerRadius="80%"
+            dataKey="count"
+          >
+            {byAgeList.map((eachItem, index) => (
+              <Cell
+                name={eachItem.age}
+                fill={colorsList[index]}
+                key={eachItem.count}
+              />
+            ))}
+          </Pie>
+          <Legend
+            iconType="circle"
+            layout="vertical"
+            verticalAlign="middle"
+            align="bottom"
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
   )
 }
 
