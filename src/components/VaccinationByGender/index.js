00a45ba650cda2275fe143d8b8f50dd58a8bb94a
nagadurgaprasadkotthuru <125784996+nagadurgaprasadkotthuru@users.noreply.github.com>
2023-05-04 18:54:56 +0530

    fifth_commit

--- src/components/VaccinationByGender/index.js
+++ src/components/VaccinationByGender/index.js
@@ -1,5 +1,5 @@
 // Write your code here
-import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
+import {PieChart, Pie, Legend, Cell} from 'recharts'
 
 import './index.css'
 
@@ -8,30 +8,28 @@ const VaccinationByGender = props => {
   return (
     <div className="vaccination-by-age-container">
       <h1 className="heading">Vaccination by gender</h1>
-      <ResponsiveContainer width="100%" height={300}>
-        <PieChart>
-          <Pie
-            cx="50%"
-            cy="50%"
-            data={byGenderList}
-            startAngle={180}
-            endAngle={0}
-            innerRadius="40%"
-            outerRadius="70%"
-            dataKey="count"
-          >
-            <Cell name={byGenderList[0].gender} fill="#f54394" />
-            <Cell name={byGenderList[1].gender} fill="#5a8dee" />
-            <Cell name={byGenderList[2].gender} fill="#2cc6c6" />
-          </Pie>
-          <Legend
-            iconType="circle"
-            layout="horizontal"
-            verticalAlign="bottom"
-            align="center"
-          />
-        </PieChart>
-      </ResponsiveContainer>
+      <PieChart width={1000} height={300}>
+        <Pie
+          cx="50%"
+          cy="50%"
+          data={byGenderList}
+          startAngle={180}
+          endAngle={0}
+          innerRadius="40%"
+          outerRadius="70%"
+          dataKey="count"
+        >
+          <Cell name={byGenderList[0].gender} fill="#f54394" />
+          <Cell name={byGenderList[1].gender} fill="#5a8dee" />
+          <Cell name={byGenderList[2].gender} fill="#2cc6c6" />
+        </Pie>
+        <Legend
+          iconType="circle"
+          layout="horizontal"
+          verticalAlign="bottom"
+          align="center"
+        />
+      </PieChart>
     </div>
   )
 }
